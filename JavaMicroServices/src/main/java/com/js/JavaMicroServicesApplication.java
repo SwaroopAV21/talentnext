3affb30bf6e6abe4e1428d91b932391886de3a65
Swaroop A V <swaroopav0303@gmail.com>
2023-08-23 20:06:17 +0530

    Day2 Tasks done

--- JavaMicroServices/src/main/java/com/js/JavaMicroServicesApplication.java
+++ JavaMicroServices/src/main/java/com/js/JavaMicroServicesApplication.java
@@ -1,13 +1,30 @@
 package com.js;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.js.entity.JEntity;
+import com.js.repo.JsRepo;
+
 @SpringBootApplication
-public class JavaMicroServicesApplication {
+public class JavaMicroServicesApplication implements CommandLineRunner{
+
+	@Autowired
+    @Qualifier(value="jsRepo")
+    private JsRepo jr;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaMicroServicesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		jr.save(new JEntity(null,"Tv",25000));
+		jr.save(new JEntity(null,"Mouse",2500));
+		jr.save(new JEntity(null,"Monitor",50000));
 	}
 
 }
--- JavaMicroServices/src/main/java/com/js/entity/JEntity.java
+++ JavaMicroServices/src/main/java/com/js/entity/JEntity.java
@@ -2,18 +2,25 @@ package com.js.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="jsproduct")
 public class JEntity {
     
     @Id
     @Column(name ="pid")
-    public int pid;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer pid;
 
     @Column(name="product_name")
     public String pname;
