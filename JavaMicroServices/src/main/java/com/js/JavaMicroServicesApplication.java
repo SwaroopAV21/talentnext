5bfbd704aebe4a87755205d5c6b59fdfe9153fd7
Swaroop A V <swaroopav0303@gmail.com>
2023-08-24 13:43:00 +0530

    Eureka Client added

--- JavaMicroServices/src/main/java/com/js/JavaMicroServicesApplication.java
+++ JavaMicroServices/src/main/java/com/js/JavaMicroServicesApplication.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import com.js.entity.JEntity;
 import com.js.repo.JsRepo;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class JavaMicroServicesApplication implements CommandLineRunner{
 
