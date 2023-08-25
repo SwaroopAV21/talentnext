c2c70b629b68684968748ee4bb71d06531a10858
Swaroop A V <swaroopav0303@gmail.com>
2023-08-25 13:05:38 +0530

    Minor changes

--- Eureka-OpenFeing-Hystrix/src/main/java/com/ofg/EurekaOpenFeingHystrix/EurekaOpenFeingHystrixApplication.java
+++ Eureka-OpenFeing-Hystrix/src/main/java/com/ofg/EurekaOpenFeingHystrix/EurekaOpenFeingHystrixApplication.java
@@ -2,7 +2,9 @@ package com.ofg.EurekaOpenFeingHystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class EurekaOpenFeingHystrixApplication {
 
@@ -11,3 +13,12 @@ public class EurekaOpenFeingHystrixApplication {
 	}
 
 }
+
+//annotations these are required for Exeption handling and the spring boot version should be under 2.4.10.and the dependency 
+// 			<dependency>
+// 				<groupId>org.springframework.cloud</groupId>
+// 				<artifactId>spring-cloud-starter-netflix-hystrix</artifactId>
+// 			</dependency>
+//@EnableFeignClients
+//@EnableCircuitBreaker
+//@EnableHystrixDashboard
